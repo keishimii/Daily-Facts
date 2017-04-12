fde3af1621cd35e9b54bb698132748c798e6894a
Keishi Mii <kam140430@utdallas.edu>
2017-04-12 00:37:20 -0500

    clean up

--- dailyFacts.py
+++ dailyFacts.py
@@ -1,17 +1,36 @@
 import nexmo
 import urllib
-from appConfig import NEXMO_KEY, NEXMO_SECRET, testNumber1, contact1
+from appConfig import NEXMO_KEY, NEXMO_SECRET, nexmoNumber
 
-params = {
-    'api_key': NEXMO_KEY,
-    'api_secret': NEXMO_SECRET,
-    'to': contact1,
-    'from': testNumber1,
-    'text': 'Random facts are not all that random.'
-}
+#send SMS message to contact   
+# *need to use http1.1 in order to send batch SMS   
+def sendSMS(contact):
+        params = {
+            'api_key': NEXMO_KEY,
+            'api_secret': NEXMO_SECRET,
+            'to': contact,
+            'from': nexmoNumber,
+            'text': 'Random facts are not all that random.'
+        }
 
-url = 'https://rest.nexmo.com/sms/json?' + urllib.parse.urlencode(params)
+        url = 'https://rest.nexmo.com/sms/json?' + urllib.parse.urlencode(params)
 
-request = urllib.request.Request(url)
-request.add_header('Accept', 'application/json')
-response = urllib.request.urlopen(request)
+        request = urllib.request.Request(url)
+        request.add_header('Accept', 'application/json')
+        response = urllib.request.urlopen(request)
+        print(response)
+
+        response = None
+
+def main():
+
+    contacts = []
+    
+    #store numbers in list from contactBook.txt
+    contacts = open("contactBook.txt", 'r').read().splitlines()
+
+    for contact in contacts:
+         sendSMS(contact)
+
+if __name__ == '__main__':
+    main()
